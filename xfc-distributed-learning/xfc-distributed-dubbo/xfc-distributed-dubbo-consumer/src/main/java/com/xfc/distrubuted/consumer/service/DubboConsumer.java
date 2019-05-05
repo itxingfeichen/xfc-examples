34cb90474e233858d8383d28bf0408c6bc0c6c4a
chenxingfei <chenxingfei@mintq.com>
2019-05-05 20:03:05 +0800

    dubbo通过zookeeper作为注册中心

--- xfc-distributed-learning/xfc-distributed-dubbo/xfc-distributed-dubbo-consumer/src/main/java/com/xfc/distrubuted/consumer/service/DubboConsumer.java
+++ xfc-distributed-learning/xfc-distributed-dubbo/xfc-distributed-dubbo-consumer/src/main/java/com/xfc/distrubuted/consumer/service/DubboConsumer.java
@@ -5,6 +5,7 @@ import com.xfc.distributed.dubbo.common.RequestDto;
 import com.xfc.distributed.dubbo.common.ResponseDto;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -15,8 +16,9 @@ import java.util.UUID;
 public class DubboConsumer {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/dubbo-consumer.xml");
+        context.start();
         IDubboTestService dubboTest = (IDubboTestService) context.getBean("demoService");
         RequestDto requestDto = new RequestDto();
         requestDto.setOrderId(11L);
@@ -24,5 +26,6 @@ public class DubboConsumer {
         ResponseDto responseDto = dubboTest.helloDubbo(requestDto);
         System.out.println("requestDto = " + responseDto);
 
+//        System.in.read();
     }
 }
