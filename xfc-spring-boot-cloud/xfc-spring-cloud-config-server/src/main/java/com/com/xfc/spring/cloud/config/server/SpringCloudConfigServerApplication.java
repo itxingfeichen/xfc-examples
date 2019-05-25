42e72a6e98525c71f318df0bc70ecf10bc659714
chenxingfei <chenxingfei@mintechai.com>
2019-05-25 21:17:28 +0800

    配置中心提交

--- xfc-spring-boot-cloud/xfc-spring-cloud-config-server/src/main/java/com/com/xfc/spring/cloud/config/server/SpringCloudConfigServerApplication.java
+++ xfc-spring-boot-cloud/xfc-spring-cloud-config-server/src/main/java/com/com/xfc/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -1,6 +1,8 @@
 package com.com.xfc.spring.cloud.config.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,4 +13,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class);
+        System.out.println("app start");
+    }
 }
---
+++ xfc-spring-boot-cloud/xfc-spring-cloud-config-server/src/main/java/com/com/xfc/spring/cloud/config/server/controller/ServerController.java
@@ -0,0 +1,22 @@
+//package com.com.xfc.spring.cloud.config.server.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// * @author : chenxingfei
+// * @date: 2019-05-25  19:16
+// * @description: 配置服务测试
+// */
+//@Controller
+//public class ServerController {
+//
+//    @Value("${my.name}")
+//    private String myName;
+//
+//    @GetMapping("/index")
+//    public String index(){
+//        return myName;
+//    }
+//}
