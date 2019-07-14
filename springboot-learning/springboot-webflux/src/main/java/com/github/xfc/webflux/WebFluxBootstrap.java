1577594ab1ae7c23a23d5e1dad4a70bad2e4c065
chenxingfei <chenxingfei@mintechai.com>
2019-07-14 20:17:14 +0800

    ResponseResult测试

---
+++ springboot-learning/springboot-webflux/src/main/java/com/github/xfc/webflux/WebFluxBootstrap.java
@@ -0,0 +1,37 @@
+package com.github.xfc.webflux;
+
+import reactor.core.publisher.Flux;
+
+/**
+ * @author : chenxingfei
+ * @date: 2019-07-14  19:39
+ * @description: 启动类
+ */
+public class WebFluxBootstrap {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread().getName());
+
+        Flux<String> flux = Flux.generate( () -> 0,
+                (value, sink) -> {
+                    sink.next("value : " + value);
+                    if (value == 10) sink.complete();
+                    return value + 1;
+                });
+
+        flux.subscribe(value-> System.out.println(Thread.currentThread().getName() +"  " + value));
+
+        Flux.range(0, 10).handle((item, sink) -> {
+            if (item % 2 == 0) {
+                sink.next("Even : " + item);
+            }
+        }).subscribe(value-> System.out.println(value));
+
+    }
+
+
+
+
+}
