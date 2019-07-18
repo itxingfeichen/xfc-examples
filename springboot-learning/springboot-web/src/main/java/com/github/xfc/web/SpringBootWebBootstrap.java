1a29a26468d4e57dd511483fb54314c6e6348caf
chenxingfei <chenxingfei@mintechai.com>
2019-07-18 08:05:50 +0800

    servlet异步测试

---
+++ springboot-learning/springboot-web/src/main/java/com/github/xfc/web/SpringBootWebBootstrap.java
@@ -0,0 +1,18 @@
+package com.github.xfc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : chenxingfei
+ * @date: 2019-07-18  07:52
+ * @description: 启动类
+ */
+@SpringBootApplication
+public class SpringBootWebBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebBootstrap.class);
+    }
+
+}
