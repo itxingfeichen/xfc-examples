16f4e80d4be49ea658e54e3edc1c2d0f8172e688
chenxingfei <chenxingfei@mintechai.com>
2019-08-03 20:10:35 +0800

    java条件装配

--- springboot-learning/springboot-web/src/main/java/com/github/xfc/web/SpringBootWebBootstrap.java
+++ springboot-learning/springboot-web/src/main/java/com/github/xfc/web/SpringBootWebBootstrap.java
@@ -1,6 +1,7 @@
 package com.github.xfc.web;
 
 import com.github.xfc.web.service.CalculatingService;
+import com.github.xfc.web.service.Java12CalculatingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -19,6 +20,12 @@ public class SpringBootWebBootstrap {
         CalculatingService bean = run.getBean(CalculatingService.class);
 
         System.out.println("求和结果" + bean.doSum(1, 2, 3, 4, 5));
+
+        Java12CalculatingService java12CalculatingService = run.getBean(Java12CalculatingService.class);
+
+        // 为了测试spring.profiles.active多个值用法
+        System.out.println(java12CalculatingService.doSum(1, 2, 3, 4, 5));
+
     }
 
 }
