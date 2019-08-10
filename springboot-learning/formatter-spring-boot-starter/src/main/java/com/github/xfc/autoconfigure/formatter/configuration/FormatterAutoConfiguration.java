bde70eea593a7a2960b54d78870991c2dd36dce0
chenxingfei <chenxingfei@mintechai.com>
2019-08-10 15:48:42 +0800

    自定义starter的bean是否存在问题

--- springboot-learning/formatter-spring-boot-starter/src/main/java/com/github/xfc/autoconfigure/formatter/configuration/FormatterAutoConfiguration.java
+++ springboot-learning/formatter-spring-boot-starter/src/main/java/com/github/xfc/autoconfigure/formatter/configuration/FormatterAutoConfiguration.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.xfc.autoconfigure.formatter.DefaultFormatter;
 import com.github.xfc.autoconfigure.formatter.Formatter;
 import com.github.xfc.autoconfigure.formatter.JsonFormatter;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,8 @@ import org.springframework.context.annotation.Configuration;
  * @description: formatter auto configuration
  */
 @Configuration
+// 添加这个注解可以解决FormatterAutoConfiguration比JacksonAutoConfiguration优先初始化的问题
+//@AutoConfigureAfter(value = {JacksonAutoConfiguration.class})
 public class FormatterAutoConfiguration {
 
     /**
