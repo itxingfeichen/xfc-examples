8391bb7a49e35f296918941955c3d4629757efdd
jannik <xingfeichen@163.com>
2019-12-12 06:58:18 +0800

    增加数据结构和算法模块

--- xfc-current-learning/src/main/java/com/github/xfc/lock/LockTest.java
+++ xfc-current-learning/src/main/java/com/github/xfc/lock/LockTest.java
@@ -18,4 +18,43 @@ public class LockTest {
             System.out.println("true = " + true);
     }
 
+    public static void main(String[] args) throws Exception {
+
+//        System.out.println(3 + (3 >> 1));
+//        ClassB classB = new ClassB();
+
+        split("我ABC",3);
+
+    }
+
+
+    public static class ClassA{
+        private ClassA(){
+            System.out.println("ClassA init....");
+        }
+
+        private void method(){
+            System.out.println("method();");
+        }
+    }
+
+    public static class ClassB extends ClassA{
+
+        public ClassB(){
+//            super();
+        }
+    }
+
+    public static void split(String source,int num) throws Exception
+    {
+        int k=0;
+        String temp="";
+        for (int i = 0; i <source.length(); i++) {
+            byte[] b=(source.charAt(i)+"").getBytes(); k=k+b.length;
+            if(k>num)
+            {
+                break; }
+            temp=temp+source.charAt(i); }
+        System.out.println(temp); }
+
 }
