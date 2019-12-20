1686cc56e3e43ac6828c518d2ba9a91568709db4
jannik <xingfeichen@163.com>
2019-12-20 08:08:48 +0800

    插入排序逻辑

--- xfc-algorithm/src/main/java/com/xfc/structure/sort/InsertSort.java
+++ xfc-algorithm/src/main/java/com/xfc/structure/sort/InsertSort.java
@@ -18,9 +18,10 @@ public class InsertSort implements Sort {
      */
     @Override
     public int[] doSort(int[] data) {
+        int tmp;
         for (int i = 1; i < data.length; i++) {
             if (data[i] < data[i - 1]) {
-                int tmp = data[i];
+                tmp = data[i];
                 for (int j = i; j >= 0; j--) {
                     if (j > 0 && tmp < data[j - 1]) {
                         data[j] = data[j - 1];
