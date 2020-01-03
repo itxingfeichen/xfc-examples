69f5e4363ca5b3abbb9f673c000fd2d64297ab0d
jannik <xingfeichen@163.com>
2020-01-03 08:07:36 +0800

    二叉树遍历

---
+++ xfc-algorithm/src/main/java/com/xfc/structure/tree/MyTree.java
@@ -0,0 +1,82 @@
+package com.xfc.structure.tree;
+
+/**
+ * 二叉树操作
+ *
+ * @author jannik
+ * @date 2020-01-01
+ */
+public class MyTree<V> {
+
+
+    public static void main(String[] args) {
+        TreeNode<Integer> treeNode = new TreeNode<>(1);
+        TreeNode<Integer> node1 = new TreeNode<>(2);
+        TreeNode<Integer> node2 = new TreeNode<>(3);
+        TreeNode<Integer> node3 = new TreeNode<>(4);
+        TreeNode<Integer> node4 = new TreeNode<>(5);
+        treeNode.setLeft(node1);
+        node1.setLeft(node2);
+        node1.setRight(node3);
+        treeNode.setRight(node4);
+        MyTree<Integer> myTree = new MyTree<>();
+        myTree.preTraversal(treeNode);
+
+    }
+
+
+    /**
+     * 前序遍历
+     */
+    private void preTraversal(TreeNode<V> treeNode) {
+        if (treeNode == null) {
+            System.out.println("此树空树");
+            return;
+        }
+        //
+        System.out.println(treeNode.getData());
+        if (treeNode.getLeft() != null) {
+            preTraversal(treeNode.getLeft());
+        }
+        if (treeNode.getRight() != null) {
+            preTraversal(treeNode.getRight());
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    private void medilleTraversal(TreeNode<V> treeNode) {
+        if (treeNode == null) {
+            System.out.println("此树空树");
+            return;
+        }
+        //
+        if (treeNode.getLeft() != null) {
+            preTraversal(treeNode.getLeft());
+        }
+        System.out.println(treeNode.getData());
+        if (treeNode.getRight() != null) {
+            preTraversal(treeNode.getRight());
+        }
+    }
+    /**
+     * 前序遍历
+     */
+    private void postTraversal(TreeNode<V> treeNode) {
+        if (treeNode == null) {
+            System.out.println("此树空树");
+            return;
+        }
+        //
+        if (treeNode.getLeft() != null) {
+            preTraversal(treeNode.getLeft());
+        }
+        if (treeNode.getRight() != null) {
+            preTraversal(treeNode.getRight());
+        }
+        System.out.println(treeNode.getData());
+    }
+
+
+}
---
+++ xfc-algorithm/src/main/java/com/xfc/structure/tree/TreeNode.java
@@ -0,0 +1,21 @@
+package com.xfc.structure.tree;
+
+import lombok.Data;
+
+/**
+ * @author jannik
+ * @date 2020-01-02
+ */
+@Data
+public class TreeNode<V> {
+
+    private TreeNode<V> left;
+
+    private TreeNode<V> right;
+
+    private V data;
+
+    public TreeNode(V data) {
+        this.data = data;
+    }
+}
