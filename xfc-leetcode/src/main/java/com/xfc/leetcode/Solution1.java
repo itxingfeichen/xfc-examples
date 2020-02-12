29cb97bb799cf455d55dacb7ec34e22989546045
jannik <xingfeichen@163.com>
2020-02-12 21:42:23 +0800

    leetcode代码

---
+++ xfc-leetcode/src/main/java/com/xfc/leetcode/Solution1.java
@@ -0,0 +1,50 @@
+package com.xfc.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * leetcode题目1
+ * 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author jannik
+ * @date 2020-02-12
+ */
+public class Solution1 {
+
+    /**
+     * 两数之和
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp)) {
+                return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+
+        }
+        throw new NullPointerException("no data");
+
+    }
+
+}
